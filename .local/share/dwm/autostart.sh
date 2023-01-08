8a99f40232b77c9a7862b89c7d9e40de7b1d568a
mjkloeckner <martin.cachari@gmail.com>
2023-01-08 15:33:48 -0300

    updated dwm autostart script

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,21 +1,39 @@
-# set wallpaper
-# nitrogen --restore
+# set proper resolution
+# xrandr -s 1366x768
 
-# Start compositor
+# start compositor
 # picom &
-picom --experimental-backends --backend glx &
 
-#$HOME/.config/dwm/dwm-bar/dwm_bar.sh &
+unclutter &
 
-xbindkeys -f $HOME/.config/xbindkeys/.xbindkeysrc &
+# swaps lock-mayus with escape & lalt with lsuper;
+# keyboardmap &
 
-keyboardmap &
-xinput --set-prop 12 'libinput Tapping Enabled' 1
+# xbindkeys -f $HOME/.config/xbindkeys/.xbindkeysrc &
 
-# lockscreen &
 xss-lock -l lockscreen &
 
-# xrandr --output HDMI1 --mode 1600x900 --above eDP1 && nitrogen --restore &
+# i3lock -c 00000000 --wrongtext="" --veriftext="" --veriftext="" --noinputtext="" -i $HOME/.cache/i3lock/current/dimblur.png --ringcolor=8B008B --keyhlcolor=ff00ff --verifcolor=0000FF --indicator
 
-# setxkbmap -option caps:swapescape
-# setxkbmap -option altwin:swap_lalt:lwin
+# xrandr --output HDMI1 --mode 1600x900 --above eDP1 && nitrogen --restore &
+# export AWT_TOOLKIT=MToolkit &
+
+# xss-lock grabs a logind suspend inhibit lock and will use betterlockscreen to lock the screen before suspend.
+# xss-lock -l lockscreen &
+
+# xfce4-power-manager &
+
+# polkit-dumb-agent &
+
+# $HOME/.config/dwmblocks/dwmblocks &
+export QT_QPA_PLATFORMTHEME='qt5ct'
+
+# Minimal picom:
+# picom --config /dev/null --backend xrender --experimental-backends
+# picom --experimental-backends --backend glx &
+# picom --experimental-backends --backend glx --vsync --xrender-sync-fence &
+# picom
+
+sys-dark-scheme
+
+# kill -35 $(pidof dwmblocks)
